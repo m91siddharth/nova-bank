1097e6fd87741d766972f5a36980cf744da21929
m91siddharth <siddharth.malhotra91@gmail.com>
2025-04-18 22:40:02 -0400

    nova bank application

---
+++ src/main/java/com/nova/bank/novabank/NovaBankApplication.java
@@ -0,0 +1,13 @@
+package com.nova.bank.novabank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovaBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovaBankApplication.class, args);
+	}
+
+}
